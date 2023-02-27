3a1c9f8141721258435aeda7a1fbe706cb511fc8
Pascal Honegger <pascal.honegger@bluewin.ch>
2023-02-27 13:17:05 +0100

    feat(ci): configure CI using GitHub Actions

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     application
     id("samt-core.kotlin-conventions")
+    @Suppress("DSL_SCOPE_VIOLATION") // Fixed in Gradle 8.1, see https://github.com/gradle/gradle/issues/22797
     alias(libs.plugins.shadow)
 }
 

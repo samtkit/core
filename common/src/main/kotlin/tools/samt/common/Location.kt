0ad2314fdb6d1100e9a18d7318c596a7d1103af7
Leonard Schütz <leni.schuetz@me.com>
2023-03-30 19:22:13 +0200

    refactor(misc): Use KDoc comment instead of line

--- common/src/main/kotlin/tools/samt/common/Location.kt
+++ common/src/main/kotlin/tools/samt/common/Location.kt
@@ -4,7 +4,9 @@ data class FileOffset(val charIndex: Int, val row: Int, val col: Int) {
     override fun toString(): String = "${row + 1}:${col + 1}"
 }
 
-// start and end file offsets of a location. start is inclusive, end is exclusive.
+/**
+ * start and end file offsets of a location. start is inclusive, end is exclusive.
+ * */
 data class Location(
     val start: FileOffset,
     val end: FileOffset = start

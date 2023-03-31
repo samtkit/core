c2a9ef0824b5a55f9490768f2186872ade51841f
Pascal Honegger <pascal.honegger@bluewin.ch>
2023-03-31 14:22:06 +0200

    chore(deps): update `shadow` from `8.1.0` to `8.1.1`

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ include(":parser")
 
 dependencyResolutionManagement {
     versionCatalogs {
-        val shadow = "8.1.0"
+        val shadow = "8.1.1"
         val jCommander = "1.82"
         val mordant = "2.0.0-beta12"
         val kover = "0.6.1"

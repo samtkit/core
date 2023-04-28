ceb44fd4004cd5d01da7cd49723672058b226a21
Marcel Joss <marcel.joss@ost.ch>
2023-04-28 17:16:00 +0200

    WIP: Language Server

--- language-server/src/main/kotlin/tools/samt/ls/CliArgs.kt
+++ language-server/src/main/kotlin/tools/samt/ls/CliArgs.kt
@@ -5,4 +5,14 @@ import com.beust.jcommander.Parameter
 class CliArgs {
     @Parameter(names = ["-h", "--help"], description = "Display help", help = true)
     var help: Boolean = false
+
+    @Parameter(names = ["--server-port", "-sp"])
+    var serverPort: Int? = null
+    @Parameter(names = ["--client-port", "-cp"])
+    var clientPort: Int? = null
+    @Parameter(names = ["--client-host", "-ch"])
+    var clientHost: String = "localhost"
+
+    @Parameter(names = ["--stdio"])
+    var isStdio: Boolean = false
 }
---
+++ language-server/src/main/kotlin/tools/samt/ls/SamtLanguageServer.kt
@@ -0,0 +1,36 @@
+package tools.samt.ls
+
+import org.eclipse.lsp4j.*
+import org.eclipse.lsp4j.services.*
+import java.io.Closeable
+import java.util.concurrent.CompletableFuture
+
+class SamtLanguageServer : LanguageServer, LanguageClientAware, Closeable {
+    private lateinit var client: LanguageClient
+
+    override fun initialize(params: InitializeParams): CompletableFuture<InitializeResult> = CompletableFuture.supplyAsync {
+        val capabilities = ServerCapabilities().apply {
+
+        }
+        InitializeResult(capabilities)
+    }
+
+    override fun shutdown(): CompletableFuture<Any> = CompletableFuture.completedFuture(null)
+
+    override fun exit() {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTextDocumentService(): TextDocumentService? = null
+
+    override fun getWorkspaceService(): WorkspaceService? = null
+
+    override fun connect(client: LanguageClient) {
+        this.client = client
+    }
+
+    override fun close() {
+        shutdown().get()
+    }
+
+}

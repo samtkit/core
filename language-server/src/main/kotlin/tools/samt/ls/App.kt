6c32d49838074c2ee0f383209b699aa430961c36
Marcel Joss <marcel.joss@ost.ch>
2023-04-28 17:16:00 +0200

    feat(ls): create language server

--- language-server/src/main/kotlin/tools/samt/ls/App.kt
+++ language-server/src/main/kotlin/tools/samt/ls/App.kt
@@ -1,6 +1,19 @@
 package tools.samt.ls
 
 import com.beust.jcommander.JCommander
+import org.eclipse.lsp4j.launch.LSPLauncher
+import java.io.InputStream
+import java.io.OutputStream
+import java.net.ServerSocket
+import java.net.Socket
+
+private fun startServer(inStream: InputStream, outStream: OutputStream) {
+    SamtLanguageServer().use {
+        val launcher = LSPLauncher.createServerLauncher(it, inStream, outStream)
+        it.connect(launcher.remoteProxy)
+        launcher.startListening().get()
+    }
+}
 
 fun main(args: Array<String>) {
     val cliArgs = CliArgs()
@@ -13,4 +26,28 @@ fun main(args: Array<String>) {
         jCommander.usage()
         return
     }
+
+    cliArgs.clientPort?.also { port ->
+        Socket(cliArgs.clientHost, port).use {
+            println("Connecting to client at ${it.remoteSocketAddress}:${it.port}")
+            startServer(it.inputStream, it.outputStream)
+        }
+        return
+    }
+
+    cliArgs.serverPort?.also { port ->
+        ServerSocket(port).use {
+            println("Starting server on port ${it.localPort}")
+            val socket = it.accept()
+            startServer(socket.inputStream, socket.outputStream)
+        }
+        return
+    }
+
+    if (cliArgs.isStdio) {
+        startServer(System.`in`, System.out)
+        return
+    }
+
+    jCommander.usage()
 }

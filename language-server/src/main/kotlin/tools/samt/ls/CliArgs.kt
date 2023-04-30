7098e50ba0884f81f10d1bb42cdd9cd4f3fc9ec0
Marcel Joss <marcel.joss@ost.ch>
2023-04-30 21:36:22 +0200

    feat(lsp): remove untested server-port option

--- language-server/src/main/kotlin/tools/samt/ls/CliArgs.kt
+++ language-server/src/main/kotlin/tools/samt/ls/CliArgs.kt
@@ -1,17 +1,21 @@
 package tools.samt.ls
 
 import com.beust.jcommander.Parameter
+import com.beust.jcommander.Parameters
 
+@Parameters(separators = "=")
 class CliArgs {
     @Parameter(names = ["-h", "--help"], description = "Display help", help = true)
     var help: Boolean = false
 
-    @Parameter(names = ["--server-port", "-sp"])
-    var serverPort: Int? = null
-    @Parameter(names = ["--client-port", "-cp"])
+    @Parameter(names = ["--client-host"])
+    var clientHost = "localhost"
+
+    /**
+     * Option is called Socket because that's what VS Code passes
+     */
+    @Parameter(names = ["--socket", "--client-port"])
     var clientPort: Int? = null
-    @Parameter(names = ["--client-host", "-ch"])
-    var clientHost: String = "localhost"
 
     @Parameter(names = ["--stdio"])
     var isStdio: Boolean = false
